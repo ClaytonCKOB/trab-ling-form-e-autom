bbe103e02aeeb5f46e0ab3138fc6e8820063cb83
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2023-04-06 14:15:13 -0300

    Melhorado sistema de comparação

--- comparison.py
+++ comparison.py
@@ -6,13 +6,17 @@ from state_machine_2 import StateMachine as ST2
 st1 = ST1()
 st2 = ST2("Final.jff")
 
+used_transitions = set()
 
 def compare_automata(events):
     st1.reset()
     st2.reset()
 
+    errors = []
     for i, l in enumerate(events, start=1):
         l = l.strip() # Remove '\n'
+
+        used_transitions.add((st2.current_state, l))
 
         # Run the same event on both automata
         r1 = st1.execute(l)
@@ -24,27 +28,41 @@ def compare_automata(events):
             or r1["dest_state"] != r2["dest_state"]
             or r1["error"] != r2["error"]
         ):
-            print(
-                f"    Diferença encontrada no teste {file.name}, linha {i}, evento {l}"
+            errors.append(
+                f"    Diferença encontrada: linha {i}, evento {l}\n"
+                f"    Saída Autômato: {r1}\n"
+                f"    Saída Referência: {r2}"
             )
-            print("    Saída Autômato: ", r1)
-            print("    Saída Referência: ", r2)
-            return False
+            return errors
     
     # Compare final state
     if st1.current_state != st2.states[st2.current_state]:
-        print(
+        errors.append(
             f"Automatos terminaram em estados diferentes: {st1.current_state} e {st2.states[st2.current_state]}"
         )
-        return False
-    return True
+        return errors
+    return errors
 
 # Run all tests
 tests = pathlib.Path(__file__).parent / "tests"
 for file in tests.iterdir():
     with file.open() as events:
-        if compare_automata(events):
-            output = "OK"
-        else:
+        if (errors := compare_automata(events)):
             output = "ERRO"
+        else:
+            output = "OK"
     print(f"Teste '{file.name}': {output}")
+    
+    for error in errors:
+        print(error)
+
+print()
+
+untested = st2.transitions.keys() - used_transitions
+if untested:
+    print(f"Transições não testadas: {len(untested)}")
+    for transition in sorted(untested):
+        state, event = transition
+        print(f"- {st2.states[state]}, {event}")
+else:
+    print("Todas as transições foram testadas")
